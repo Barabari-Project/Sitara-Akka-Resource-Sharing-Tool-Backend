66f28c95a9e6d268ef378d8fd47ff27181c781b1
drumil32 <akheniad@gmail.com>
2025-06-18 22:00:07 +0530

    [fix]: dep issue

--- dist/index.js
+++ dist/index.js
@@ -15,7 +15,9 @@ const auth_middleware_1 = require("./middleware/auth.middleware");
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
-app.use((0, cors_1.default)());
+app.use((0, cors_1.default)({
+    origin: '*',
+}));
 (0, database_1.connectToDatabase)();
 app.use('/api', get_route_1.getRouter);
 app.use('/api', auth_route_1.authRouter);
