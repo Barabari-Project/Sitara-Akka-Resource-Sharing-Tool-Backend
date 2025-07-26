7535ff949dacb5da6311822ad002d65579960316
Jash Gusani <jashgusani78@gmail.com>
2025-07-26 23:50:25 +0530

    Delete not needed Item

--- dist/constants/wpEndPoint.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.wpEndPoint = void 0;
-exports.wpEndPoint = {
-    userStatus: '/api/athena/chat/status',
-    sendMedia: '/api/athena/messages/media',
-    openWhatsAppWindow: '/api/athena/messages/template',
-    uploadFile: '/api/athena/media/upload',
-};
